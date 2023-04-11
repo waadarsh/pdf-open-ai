1a1053850be1275b44d60175b4e662d5c1576ad1
Aadarsh Unni Wilson <waadarsh@outlook.com>
2023-04-11 22:25:27 +0530

    Window to show Chat based on the current question by user

---
+++ src/components/ResultSection.js
@@ -0,0 +1,24 @@
+import { Message } from "semantic-ui-react";
+
+function ResultSection({ searchResults }) {
+  return (
+    <>
+      <Message style={{ height: "100%", overflowY: "scroll" }}>
+        <Message.Header>Result Window</Message.Header>
+        <Message.List>
+          {searchResults
+            .slice()
+            .reverse()
+            .map((result) => (
+              <Message.Item>
+                <h4>{result.input}</h4> <p>{result.response}</p>
+                <br></br>
+              </Message.Item>
+            ))}
+        </Message.List>
+      </Message>
+    </>
+  );
+}
+
+export default ResultSection;
