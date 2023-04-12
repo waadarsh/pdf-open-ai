6ad6b6be02ee8977c568e1194aea71a520dd775d
Aadarsh Unni Wilson <waadarsh@outlook.com>
2023-04-12 09:55:19 +0530

    DataBase connected

--- src/components/HistorySection.js
+++ src/components/HistorySection.js
@@ -1,14 +1,32 @@
-import { Grid, Segment } from "semantic-ui-react";
+import React, { useState, useEffect } from 'react';
+import { List } from 'semantic-ui-react';
+import axios from 'axios';
 
 function HistorySection() {
+  const [searchHistory, setSearchHistory] = useState([]);
+
+  useEffect(() => {
+    // Fetch search history from the Flask API
+    axios.get('http://localhost:8000/history').then((response) => {
+      console.log(response);
+      setSearchHistory(response.data || []);
+    });
+  }, []);
+
   return (
     <>
-      <Grid.Row>
-        <Segment textAlign="center">New Chat</Segment>
-      </Grid.Row>
-      <Grid.Row>
-        <Segment textAlign="center">Chat Archives</Segment>
-      </Grid.Row>
+      <h3>Search History</h3>
+      <List>
+        {searchHistory.map((search, index) => (
+          // rome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+          <List.Item key={index}>
+            <List.Content>
+              <List.Header>{search.input}</List.Header>
+              {/* <List.Description>{search.response}</List.Description> */}
+            </List.Content>
+          </List.Item>
+        ))}
+      </List>
     </>
   );
 }
